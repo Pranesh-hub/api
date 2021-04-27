2af2b08826cd27bc8dfc245fdea9970334cf1b3e
Pranesh S <avsenthilvel@gmail.com>
2021-04-27 14:26:56 +0530

    Sync 2

---
+++ .vs/api/update.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import base64
+import time
+
+response = requests.get('http://dataservice.accuweather.com/currentconditions/v1/2807435?apikey=kaGDbpVPVB7jYAATABechNo85ZcGApy7')
+x = response.json()
+y = json.dumps(x)
+z = json.loads(y)
+var = z[0]['HasPrecipitation']
+precipitation = str(var)
+c_temp = str(z[0]["Temperature"]["Metric"]["Value"])
+weather = z[0]["WeatherText"]
+while (1>0):
+    file = open("data.txt","w")
+
+    file.write("Temperature:\n"+c_temp+"\nWeather:\n"+weather)
+
+    file.close()
+
+    print("Changed")
+
+    time.sleep(600)
